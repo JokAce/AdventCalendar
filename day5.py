b049b27772192d440b54c8680083f24e1ba84ad5
panthe <baptiste.k03@gmail.com>
2024-12-07 01:36:08 +0100

    day 5 star 2

--- day5.py
+++ day5.py
@@ -29,8 +29,6 @@ def extractUpdates(file):
 
     return result
 
-
-
 def rightUpdates(rules, updates):
     for i in range(len(updates)):
         if updates[i] in rules:
@@ -50,17 +48,55 @@ def totalRightUpdates(rules, updates):
 
     return sum
 
-def correctWrongUpdates():
-    pass
+def correctWrongUpdates(rules, updates):
 
-def totalCorrectedWrongUpdates():
-    pass
+    sum = 0
 
+    for update in updates:
+        if rightUpdates(rules, update) == 0:
+            gr = dfs(update[0], graph(update, rules))
+            sum += gr[len(gr) // 2]
+            print(gr)
+
+    return sum
+
+def graph(update, rules):
+    gr = {page: [] for page in update}
+
+    for i in range(len(update)):
+        for j in range(i + 1, len(update)):
+            if update[i] in rules and update[j] in rules[update[i]]:
+                gr[update[i]].append(update[j])
+
+    print(gr)
+
+    return gr
+
+def dfs(node, graph, visited=None, stack=None):
+    if visited is None:
+        visited = set()
+    if stack is None:
+        stack = []
+
+    def visit(node):
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph.get(node, []):
+                visit(neighbor)
+            stack.append(node)
+
+    for node in graph:
+        visit(node)
+
+    return stack[::-1]
 
 def day5():
 
     rules = extractRules("inputs/day5_input.txt")
     updates = extractUpdates("inputs/day5_input.txt")
 
-    test = totalRightUpdates(rules,updates)
-    return test
+    return correctWrongUpdates(rules, updates)
+
+
+
+    #test = correctWrongUpdates(rules,updates)
